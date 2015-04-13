33f6d59d573ca42b319ae1047de0046a83ec32f4
Nichelle Fair <nichellefair@Nichelles-MacBook-Pro.local>
2015-04-13 16:35:18 -0700

    first draft of layout,all functionality established

--- test.js
+++ test.js
@@ -1,1 +1,36 @@
-/* JS goes here */
+
+
+/* responsive nav */
+
+$(function() {
+	var pull 		= $('.pull');
+		menu 		= $('nav ul');
+		menuHeight	= menu.height();
+
+	$('.pull').on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 320 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+});
+
+/* toggle sidebar */
+
+$(document).ready(function() {
+	$('#sideBar').click(function(e) {
+		e.preventDefault();
+			$('.sidebar').toggle('slow');
+	});
+});
+
+// $(function(){
+// 	$('.nav-list').selected(function(){
+// 		$('.active').addClass();
+// 	});
+// });
